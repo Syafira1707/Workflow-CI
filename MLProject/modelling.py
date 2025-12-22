9804a3907812bca3bc55eb4aeaca3b5ba8e18ff0
Syafira Rahma Azzahra <syafirarahmaaa@gmail.com>
2025-12-23 02:04:49 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,14 +1,12 @@
 import pandas as pd
 import mlflow
 import mlflow.sklearn
-
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
-
 # =========================
-# 1. Load dataset
+# Load dataset
 # =========================
 df = pd.read_csv("titanic_preprocessed.csv")
 
@@ -19,24 +17,28 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-
 # =========================
-# 2. Train model
+# Training model
+# (MLflow Project SUDAH membuat run)
 # =========================
 model = LogisticRegression(max_iter=200)
 model.fit(X_train, y_train)
 
-
 # =========================
-# 3. Evaluation
+# Evaluasi
 # =========================
 y_pred = model.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 
+# =========================
+# Logging ke MLflow
+# =========================
+mlflow.log_metric("accuracy", accuracy)
 
-# =========================
-# 4. MLflow logging
-# (RUN SUDAH AKTIF DARI MLflow Project)
-# =========================
-mlflow.log_param("model_type", "LogisticRegression")
-mlflow.log_param("max_iter"
+# SIMPAN MODEL (PASTI ADA DI ARTIFACTS)
+mlflow.sklearn.log_model(
+    sk_model=model,
+    artifact_path="model"
+)
+
+print("Training CI via MLflow Project BERHASIL.")
