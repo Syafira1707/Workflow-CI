0821f09d9d23fe78152af123fd69f17e049e2788
Syafira Rahma Azzahra <syafirarahmaaa@gmail.com>
2025-12-22 23:59:17 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,12 +1,14 @@
-import pandas as pd
 import mlflow
 import mlflow.sklearn
-
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
-# Load dataset
+# set experiment (boleh, aman)
+mlflow.set_experiment("CI_Titanic_Model")
+
+# load dataset preprocessing
 df = pd.read_csv("titanic_preprocessed.csv")
 
 X = df.drop("Survived", axis=1)
@@ -16,16 +18,13 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-mlflow.set_experiment("CI_Titanic_Model")
+# autolog (WAJIB untuk basic)
+mlflow.autolog()
 
-with mlflow.start_run():
-    model = LogisticRegression(max_iter=1000)
-    model.fit(X_train, y_train)
+model = LogisticRegression(max_iter=1000)
+model.fit(X_train, y_train)
 
-    y_pred = model.predict(X_test)
-    acc = accuracy_score(y_test, y_pred)
+y_pred = model.predict(X_test)
+acc = accuracy_score(y_test, y_pred)
 
-    mlflow.log_metric("accuracy", acc)
-    mlflow.sklearn.log_model(model, "model")
-
-    print("Training selesai. Accuracy:", acc)
+print("Accuracy:", acc)
