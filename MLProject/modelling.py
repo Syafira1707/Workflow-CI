f1493ed6bf93eaafda5a0baf60ba62e235ca3dd4
Syafira Rahma Azzahra <syafirarahmaaa@gmail.com>
2025-12-23 01:57:42 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,12 +1,14 @@
 import pandas as pd
 import mlflow
 import mlflow.sklearn
+
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
+
 # =========================
-# Load dataset
+# 1. Load dataset
 # =========================
 df = pd.read_csv("titanic_preprocessed.csv")
 
@@ -17,27 +19,24 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
+
 # =========================
-# Training model
+# 2. Train model
 # =========================
 model = LogisticRegression(max_iter=200)
 model.fit(X_train, y_train)
 
+
 # =========================
-# Evaluasi
+# 3. Evaluation
 # =========================
 y_pred = model.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 
-# =========================
-# Logging ke MLflow
-# =========================
-mlflow.log_metric("accuracy", accuracy)
 
-# SIMPAN MODEL
-mlflow.sklearn.log_model(
-    sk_model=model,
-    artifact_path="model"
-)
-
-print("Training CI via MLflow Project BERHASIL.")
+# =========================
+# 4. MLflow logging
+# (RUN SUDAH AKTIF DARI MLflow Project)
+# =========================
+mlflow.log_param("model_type", "LogisticRegression")
+mlflow.log_param("max_iter"
