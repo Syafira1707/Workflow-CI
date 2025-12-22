592a18390d6ca5d52cb466001f61afb76ded2aef
Syafira Rahma Azzahra <syafirarahmaaa@gmail.com>
2025-12-23 02:10:35 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import mlflow
 import mlflow.sklearn
+
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
+
 
 # =========================
 # Load dataset
@@ -17,28 +19,29 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
+
 # =========================
-# Training model
-# (MLflow Project SUDAH membuat run)
+# Train model
 # =========================
 model = LogisticRegression(max_iter=200)
 model.fit(X_train, y_train)
 
-# =========================
-# Evaluasi
-# =========================
-y_pred = model.predict(X_test)
-accuracy = accuracy_score(y_test, y_pred)
 
 # =========================
-# Logging ke MLflow
+# Evaluate
 # =========================
+accuracy = accuracy_score(y_test, model.predict(X_test))
+
+
+# =========================
+# MLflow logging
+# (RUN SUDAH ADA DARI MLflow Project)
+# =========================
+mlflow.log_param("model", "LogisticRegression")
+mlflow.log_param("max_iter", 200)
+
 mlflow.log_metric("accuracy", accuracy)
 
-# SIMPAN MODEL (PASTI ADA DI ARTIFACTS)
-mlflow.sklearn.log_model(
-    sk_model=model,
-    artifact_path="model"
-)
+mlflow.sklearn.log_model(model, "model")
 
-print("Training CI via MLflow Project BERHASIL.")
+print("CI training selesai dengan sukses.")
