0fb4c569f80077097352e6e6a23bf97f4e6ce546
Syafira Rahma Azzahra <syafirarahmaaa@gmail.com>
2025-12-23 18:31:53 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -37,55 +37,31 @@ def train_model(data_path: str):
     mlflow.set_tracking_uri("file:./mlruns")
     mlflow.set_experiment("Titanic_RandomForest_Manual")
 
-    with mlflow.start_run(run_name="rf_manual_run"):
-        # ===============================
-        # 4. TRAIN MODEL
-        # ===============================
-        model = RandomForestClassifier(
-            n_estimators=100,
-            random_state=42
-        )
-        model.fit(X_train, y_train)
+    # ===============================
+    # 4. TRAIN MODEL
+    # ===============================
+    model = RandomForestClassifier(
+        n_estimators=100,
+        random_state=42
+    )
+    model.fit(X_train, y_train)
 
-        # ===============================
-        # 5. EVALUATION
-        # ===============================
-        y_pred = model.predict(X_test)
+    # ===============================
+    # 5. EVALUATION
+    # ===============================
+    y_pred = model.predict(X_test)
+    acc = accuracy_score(y_test, y_pred)
 
-        acc = accuracy_score(y_test, y_pred)
+    # ===============================
+    # 6. LOG METRICS & PARAMS
+    # ===============================
+    mlflow.log_metric("accuracy", acc)
+    mlflow.log_param("model_type", "RandomForestClassifier")
+    mlflow.log_param("n_estimators", 100)
+    mlflow.log_param("test_size", 0.2)
 
-        print("Accuracy:", acc)
-        print("\nClassification Report:")
-        print(classification_report(y_test, y_pred))
-
-        # ===============================
-        # 6. LOG METRICS & PARAMS (WAJIB)
-        # ===============================
-        mlflow.log_metric("accuracy", acc)
-
-        mlflow.log_param("model_type", "RandomForestClassifier")
-        mlflow.log_param("n_estimators", 100)
-        mlflow.log_param("test_size", 0.2)
-
-        # ===============================
-        # 7. SAVE MODEL LOCAL
-        # ===============================
-        output_dir = Path("artifacts")
-        output_dir.mkdir(exist_ok=True)
-
-        model_path = output_dir / "random_forest_titanic.pkl"
-        joblib.dump(model, model_path)
-
-        # ===============================
-        # 8. LOG ARTIFACTS (INI KUNCI)
-        # ===============================
-        mlflow.log_artifact(model_path)
-
-        print(f"\nModel disimpan di: {model_path}")
-
-
-if __name__ == "__main__":
-    BASE_DIR = Path(__file__).resolve().parent
-    DATA_PATH = BASE_DIR / "titanic_preprocessed.csv"
-
-    train_model(DATA_PATH)
+    # ===============================
+    # 7. SAVE & LOG MODEL
+    # ===============================
+    joblib.dump(model, model_path)
+    mlflow.log_artifact(model_path)
